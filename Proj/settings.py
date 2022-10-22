f7029a2b463014c7a78f4646d926aad4c8bb9f4b
bhavishya-tiwari <bhavishyatiwari2000@gmail.com>
2022-10-22 18:47:58 +0530

    d

--- Proj/settings.py
+++ Proj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q9adz32rh&xaq5#!lw43av02+3!g69_2su_%02s42#9hxc%bme
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1:8000']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- Proj/urls.py
+++ Proj/urls.py
@@ -19,9 +19,11 @@ from django.urls import path,include
 from django.conf import settings
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
+
 
 ]
 # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
